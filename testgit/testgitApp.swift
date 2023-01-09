9a58873f9f8b7868f92c46dc20e19fa805839522
nakura <nakura@office-nakura.com>
2023-01-09 16:34:28 +0900

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by nakura on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
